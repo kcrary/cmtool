7bd6bf2e4e15ea46ed35227c0dbf3308e15ac152
Karl Crary <crary@cs.cmu.edu>
2017-12-29 16:59:15 -0500

    better OCaml recursive module

--- examples-ocaml/Calc-recmod.ml
+++ examples-ocaml/Calc-recmod.ml
@@ -9,64 +9,43 @@ module Terminal =
        | RPAREN
    end
 
-module rec Arg
+module rec Lexer 
    : sig
-        type symbol = char
-        val ord : symbol -> int
-
-        type t = Terminal.terminal FStream.front
-
-        type self = { lex : symbol FStream.t -> t }
-        type info = (symbol list, symbol FStream.t, self) LexInfo.t
-
-        val eof : info -> t
-        val lparen : info -> t
-        val number : info -> t
-        val plus : info -> t
-        val rparen : info -> t
-        val times : info -> t
-        val whitespace : info -> t
-     end
-   =
-   struct
-      open FStream
-      open LexInfo
-      open Terminal
-
-      type symbol = char
-      let ord = int_of_char
-
-      type t = terminal front
-
-      type self = { lex : char stream -> t }
-      type info = (symbol list, symbol FStream.stream, self) LexInfo.t
-
-      let number { matc; follow } =
-        Cons (NUMBER (int_of_string (StringUtil.implode matc)),
-              laz (lazy (Lexer.lex follow)))
-
-      let simple terminal ({ follow }:info) =  (* this type annotation is necessary *)
-        Cons (terminal, laz (lazy (Lexer.lex follow)))
-
-      let plus = simple PLUS
-      let times = simple TIMES
-      let lparen = simple LPAREN
-      let rparen = simple RPAREN
-
-      let whitespace { follow } =
-        Lexer.lex follow
-
-      let eof _ = Nil
-    end
-
-and Lexer
-   : sig
-        val lex : Arg.symbol FStream.t -> Arg.t
+        val lex : char FStream.t -> Terminal.terminal FStream.front
      end
    =
    CalcLex.Fun
    (Streamable.StreamStreamable)
-   (Arg)
+   (struct
+       open FStream
+       open LexInfo
+       open Terminal
+ 
+       type symbol = char
+       let ord = int_of_char
+ 
+       type t = terminal front
+ 
+       type self = { lex : char stream -> t }
+       type info = (symbol list, symbol FStream.stream, self) LexInfo.t
+ 
+       let number { matc; follow } =
+         Cons (NUMBER (int_of_string (StringUtil.implode matc)),
+               laz (lazy (Lexer.lex follow)))
+ 
+       let simple terminal { follow } =
+         Cons (terminal, laz (lazy (Lexer.lex follow)))
+ 
+       let plus = simple PLUS
+       let times = simple TIMES
+       let lparen = simple LPAREN
+       let rparen = simple RPAREN
+ 
+       let whitespace { follow } =
+         Lexer.lex follow
+ 
+       let eof _ = Nil
+     end)
 
 module Parser =
    CalcParse.Fun
